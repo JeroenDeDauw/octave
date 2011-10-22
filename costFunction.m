514bab6e10837aff843fadc69f33adf6338bf4f0
Jeroen De Dauw <jeroendedauw@gmail.com>
2011-10-22 18:26:59 +0000

    tweaks

--- costFunction.m
+++ costFunction.m
@@ -1,6 +1,11 @@
-function J = costFunction( trainingMatrix, trainingVector, theta )
+#! /usr/bin/octave
+
+# trainingMatrix (X) The training matrix with examples, first column being a one-filled row vector.
+# trainingVector(y) The expected results, row vector with same row count as trainingMatrix.
+# hypothesis (theta) The 'solution' hypothesis, vector with same length as the column count of trainingMatrix.
+function J = costFunction( trainingMatrix, trainingVector, hypothesis )
 
 tariningSetSize = size( trainingMatrix, 1 );
-predictions = trainingMatrix * theta;
+predictions = trainingMatrix * hypothesis;
 
 J = sum( ( predictions - trainingVector ) .^ 2 )  / ( 2 * tariningSetSize );
