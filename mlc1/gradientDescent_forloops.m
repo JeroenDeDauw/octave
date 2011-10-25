9d9d668e023614fb6d53bb39b2e7cbd14f34bb43
Jeroen De Dauw <jeroendedauw@gmail.com>
2011-10-25 02:26:50 +0200

    made script be able to handle an arbitrary amount of features and created a vector version

--- mlc1/gradientDescent_forloops.m
+++ mlc1/gradientDescent_forloops.m
@@ -4,15 +4,30 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %   taking num_iters gradient steps with learning rate alpha
 
 
-exampleCount = length(y); % number of training examples
+m = length(y); % number of training examples
+n = length(theta);
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-
-	cost = ( X * theta - y )';
 	
-	for featureIndex = 1:length(theta)
-		theta(featureIndex) = theta(featureIndex) - alpha / exampleCount * ( cost * X(:,featureIndex) );
+	costs = zeros(n);
+	
+	for i = 1:m
+		cost = 0;
+		
+		for t = 1:n
+			cost = cost + theta(t) * X(i, t);
+		end
+		
+		cost = cost - y(i);
+		
+		for t = 1:n
+			costs(t) = costs(t) + cost * X(i, t);
+		end
+	end
+	
+	for t = 1:n
+		theta(t) = theta(t) - alpha / m * costs(t);
 	end
 	  
 	J_history(iter) = computeCost(X, y, theta);
